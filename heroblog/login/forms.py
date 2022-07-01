9db5ba58ca154296f4fc9d092ddb59e62ac60f2d
matirodriguez27 <82338146+matirodriguez27@users.noreply.github.com>
2022-07-01 18:45:48 -0300

    signup y login

--- heroblog/login/forms.py
+++ heroblog/login/forms.py
@@ -1,12 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
+from django.views.generic import CreateView
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
-    password= forms.CharField(label='Contraseña', widget = forms.PasswordInput)
+    email = forms.EmailField(widget=forms.EmailInput)
+    password1= forms.CharField(label='Contraseña', widget = forms.PasswordInput)
     password2= forms.CharField(label='Repetir contraseña', widget = forms.PasswordInput)
     class Meta:
         model = User
-        fields = ['username','email','password','password2',]
+        fields = ['username','email','password1','password2']
         help_texts = {k:"" for k in fields}
+    
+
--- heroblog/login/views.py
+++ heroblog/login/views.py
@@ -1,7 +1,8 @@
 from login.forms import UserRegisterForm
 from django.shortcuts import render
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 from django.contrib.auth import login, logout, authenticate
+
 
 def login_request(request):
     if request.method == "POST":
@@ -12,19 +13,25 @@ def login_request(request):
             user = authenticate(username=usuario,password=contrasena)
             if user is not None:
                 login(request, user)
-                return render(request, "login.html", {"mensaje":f"Bienvenido {usuario}"})
+                return render(request, "index.html", {"mensaje":f"Bienvenido {usuario}"})
             else:
-                return render(request,"index.html", {"mensaje":"Error"})
+                return render(request,"index.html", {"mensaje":"Datos incorrectos"})
         else:
-            return render(request,"index.html", {"mensaje":"Error"})
-    form = AuthenticationForm()
-    return render(request, "login.html", {'form':form})
+            return render(request,"index.html", {"mensaje":"Formulario invalido"})
+    else:
+        form = AuthenticationForm()
+        return render(request, "login.html", {'form':form})
+
 def signup(request):
     if request.method == "POST":
-        form = UserRegisterForm(request.POST)
+        form = UserCreationForm(data = request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
-            return render(request, "index.html")
+            form.save()
+            return render(request, "index.html", {"mensaje":"Usuario registrado"})
+        else:
+            return render(request,"index.html", {"mensaje":"Formulario invalido"})
     else:
-        form = UserRegisterForm()
-        return render(request, "signup.html", {'form':form})
+        form = UserCreationForm()
+        return render(request, "signup.html", {"form":form})
+
