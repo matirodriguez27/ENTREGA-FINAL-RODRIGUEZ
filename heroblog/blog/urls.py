06cf17049c4be08b69718caa7a62560e38c4d63f
matirodriguez27 <82338146+matirodriguez27@users.noreply.github.com>
2022-07-04 18:32:31 -0300

    mini cambio

--- heroblog/blog/urls.py
+++ heroblog/blog/urls.py
@@ -4,9 +4,8 @@ from blog.views import ListaPosts, DetallePost, NuevoPost, EditarPost, BorrarPos
 
 urlpatterns = [
     path('blog/', ListaPosts.as_view(), name='Lista'),
-    path('blog/lista/', ListaPosts.as_view(), name='Lista'),
-    path('blog/lista/<pk>/', DetallePost.as_view(), name='Detalle'),
+    path('blog/<pk>/', DetallePost.as_view(), name='Detalle'),
     path('blog/nuevo/', NuevoPost.as_view(), name='Nuevo'),
-    path('blog/edicion/<int:pk>/', EditarPost.as_view(), name='Editar'),
+    path('blog/edicion/<pk>/', EditarPost.as_view(), name='Editar'),
     path('blog/borrar/<pk>/', BorrarPost.as_view(), name='Borrar'),
 ]
