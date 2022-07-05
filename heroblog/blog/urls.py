e8f3b30778b976e2de3f20533eaeba1c2173d2fb
matirodriguez27 <82338146+matirodriguez27@users.noreply.github.com>
2022-07-04 22:22:36 -0300

    Funciona blog y puliendo...

--- heroblog/blog/urls.py
+++ heroblog/blog/urls.py
@@ -4,7 +4,8 @@ from blog.views import ListaPosts, DetallePost, NuevoPost, EditarPost, BorrarPos
 
 urlpatterns = [
     path('blog/', ListaPosts.as_view(), name='Lista'),
-    path('blog/<pk>/', DetallePost.as_view(), name='Detalle'),
+    path('blog/pages/', ListaPosts.as_view(), name='Lista'),
+    path('blog/pages/<pk>/', DetallePost.as_view(), name='Detalle'),
     path('blog/nuevo/', NuevoPost.as_view(), name='Nuevo'),
     path('blog/edicion/<pk>/', EditarPost.as_view(), name='Editar'),
     path('blog/borrar/<pk>/', BorrarPost.as_view(), name='Borrar'),
